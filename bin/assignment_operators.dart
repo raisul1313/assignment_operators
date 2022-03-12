3163a4bea4aaa4de8f4c71a1f522c2e6ecf90be3
raisul1313 <raisul.islam1313@gmail.com>
2022-03-13 00:37:08 +0600

    first commit

---
+++ bin/assignment_operators.dart
@@ -0,0 +1,44 @@
+void main(List<String> arguments) {
+  int a = 5;
+  print('a= : $a');
+
+  int b = 5;
+  b += 3;
+  print('b+= 3: $b');
+
+  int c = 5;
+  c -= 3;
+  print('c-= 3: $c');
+
+  int d =5;
+  d *= 3;
+  print('d*= 3: $d');
+
+  double e = 5;
+  e /= 3;
+  print('e/= 3: $e');
+
+  int f =5;
+  f %= 3;
+  print('f%= 3: $f');
+
+  int g =5;
+  g &= 3;
+  print('g&= 3: $g');
+
+  int h =5;
+  h |= 3;
+  print('h|= 3: $h');
+
+  int i =5;
+  i ^= 3;
+  print('i^= 3: $i');
+
+  int j =5;
+  j >>= 3;
+  print('j>>= 3: $j');
+
+  int k =5;
+  k <<= 3;
+  print('k<<= 3: $k');
+}
